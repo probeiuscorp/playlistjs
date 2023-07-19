7172aed0444d7fd5672c1351424dd76b5b3c108a
me <caleb.stimpson@gmail.com>
2023-07-19 13:18:19 -0500

    Loads GT Pressura Mono over https instead of http

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function PlaylistJS() {
         <Html>
             <Head>
                 <link rel="icon" href="/favicon.ico"/>
-                <link href="http://fonts.cdnfonts.com/css/gt-pressura-mono" rel="stylesheet"/>
+                <link href="https://fonts.cdnfonts.com/css/gt-pressura-mono" rel="stylesheet"/>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins&family=Inter&family=Manrope:wght@600&display=swap" rel="stylesheet"/>
