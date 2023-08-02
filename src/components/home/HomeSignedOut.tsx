8a4dd419dc900b68ab120575e21238071998cedb
me <caleb.stimpson@gmail.com>
2023-08-01 20:56:52 -0500

    Fixes typo in HomeSignedOut

--- src/components/home/HomeSignedOut.tsx
+++ src/components/home/HomeSignedOut.tsx
@@ -24,7 +24,7 @@ Playlist.yield(function*() {
         yield head;
     } while(
         head = graph.getEdgesFrom(head).pick((edge) => (
-            connection.weight
+            edge.weight
         ))
     );
 })
