47b4c75f95e866ee47c9cf76521228f312baf41d
Caleb Stimpson <caleb.stimpson@gmail.com>
2024-09-02 23:01:54 -0600

    Fix silence undefined when using playlistjs-types/shim

--- packages/playlistjs-types/prepublish.ts
+++ packages/playlistjs-types/prepublish.ts
@@ -12,12 +12,13 @@ let iShimEnd = iShimStart;
 while(content[++iShimEnd] !== '\n');
 const iShip = content.indexOf('/* ship: */');
 const shimmedContent = content.slice(0, iShimStart - 1) + content.slice(iShimEnd, iShip - 1);
-function insertFunctionsIntoGlobalThis(source: string) {
+function insertFunctionsIntoGlobalThis(source: string): string {
     while(true) {
         const match = source.match(/^function (\w+)/m);
         if(match === null) return source;
         const functionName = match[1];
-        return source.slice(0, match.index) + `globalThis.${functionName} = ` + source.slice(match.index);
+        return source.slice(0, match.index) + `globalThis.${functionName} = f`
+            + insertFunctionsIntoGlobalThis(source.slice(match.index! + 1));
     }
 }
 const minifiedContent = transformTypeScriptSync(simpleResolveFileImports(insertFunctionsIntoGlobalThis(shimmedContent), testable));
